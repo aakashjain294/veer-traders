13111735bd4a22811dd4b597b66b22dc653ecd8a
Aakash Jain <aakashjain1192000@gmail.com>
2025-04-07 20:04:59 +0530

    Adding PWA

--- workbox-config.js
+++ workbox-config.js
@@ -1,33 +1,30 @@
 module.exports = {
-    globDirectory: "build/",
-    globPatterns: [
-      "**/*.{html,js,css,json,ico,webp,png,jpg,svg}"
-    ],
-    swDest: "build/sw.js",
-    navigateFallback: "/offline.html",
-    runtimeCaching: [
-      {
-        urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
-        handler: "CacheFirst",
-        options: {
-          cacheName: "image-cache",
-          expiration: {
-            maxEntries: 100,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
+  globDirectory: "build/",
+  globPatterns: ["**/*.{html,js,css,png,jpg,jpeg,svg,json,ico}", "**/*.csv"],
+  swDest: "build/sw.js",
+  navigateFallback: "/offline.html",
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|webp)$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "image-cache",
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
         },
       },
-      {
-        urlPattern: new RegExp("https://script.google.com/macros/"),
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "google-scripts",
-          networkTimeoutSeconds: 3,
-          expiration: {
-            maxEntries: 50,
-          },
+    },
+    {
+      urlPattern: new RegExp("https://script.google.com/macros/"),
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "google-scripts",
+        networkTimeoutSeconds: 3,
+        expiration: {
+          maxEntries: 50,
         },
       },
-    ],
-  };
-  
+    },
+  ],
+};
