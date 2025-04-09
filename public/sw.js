91507ca941ca98ec435aa9f7b874936845471385
Aakash Jain <aakashjain1192000@gmail.com>
2025-04-09 17:21:28 +0530

    removed pwa

--- public/sw.js
+++ public/sw.js
@@ -9,3 +9,12 @@ self.addEventListener("install", (event) => {
     self.clients.claim();
   });
   
+  // Online-first fetch strategy
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request);
+      })
+    );
+  });
+  
--- src/index.js
+++ src/index.js
@@ -13,12 +13,3 @@ root.render(
 
 reportWebVitals();
 
-// ✅ Register Workbox Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((reg) => console.log("SW registered", reg))
-      .catch((err) => console.error("SW registration failed:", err));
-  });
-}
